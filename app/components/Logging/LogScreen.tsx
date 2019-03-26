f31b50e091c1d282ee1c48a554da4c1ff638a047
Nathan Kerr <Nathan.Kerr@nordicsemi.no>
2019-03-26 07:54:09 -0700

    Gate off the "save log" feature until it's done.

--- app/components/Logging/LogScreen.tsx
+++ app/components/Logging/LogScreen.tsx
@@ -55,6 +55,16 @@ class LogScreen extends React.Component<MyProps, {}> {
 			<Typography color="secondary" variant="subtitle1">{this.props.deviceId}</Typography>
 		) : null;
 
+		let savebtn;
+
+		if (process.env.NODE_ENV === 'development') {
+			savebtn = (
+				<IconButton color="secondary" onClick={this.saveLog} aria-label="Save">
+					<SaveIcon/>
+				</IconButton>
+			);
+		}
+
 		return (
 			<Dialog
 				fullScreen
@@ -71,9 +81,7 @@ class LogScreen extends React.Component<MyProps, {}> {
 							Logs
 						</Typography>
 						{deviceTitle}
-						<IconButton color="secondary" onClick={this.saveLog} aria-label="Save">
-							<SaveIcon />
-						</IconButton>
+						{savebtn}
 						<IconButton color="secondary" onClick={this.clearLog} aria-label="Clear">
 							<DeleteIcon/>
 						</IconButton>
