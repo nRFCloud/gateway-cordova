b8f034e24ceb57c636e5dd031229f422fe4cf74b
Nathan Kerr <Nathan.Kerr@nordicsemi.no>
2020-10-07 10:35:31 -0700

    Remove old api bundle
    Make the production build use the same config file rather than two separate ones

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
+const TerserPlugin = require('terser-webpack-plugin');
 
 const babelPresets = [[
 	require.resolve("@babel/preset-env"),
@@ -11,7 +12,7 @@ const babelPresets = [[
 		forceAllTransforms: true
 	}
 ],
-	require.resolve('@babel/preset-react'),
+require.resolve('@babel/preset-react'),
 ];
 const babelPlugins = [
 	'@babel/plugin-external-helpers',
@@ -21,78 +22,98 @@ const babelPlugins = [
 	"@babel/plugin-transform-async-to-generator"
 ].map(require.resolve);
 
-module.exports = {
-	mode: 'development',
-	entry: [
-		path.resolve(__dirname, 'helpers.js'),
-		'./app/index.tsx'
-	],
-	output: {
-		filename: 'index.js',
-		path: path.resolve(__dirname, 'www/js'),
-		libraryTarget: 'this'
-	},
-	devtool: "inline-source-map",
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules|bower_components|helpers)/,
-				loader: 'babel-loader',
-				options: {
-					presets: babelPresets,
-					plugins: babelPlugins,
-				},
-			},
-			{
-				test: /\.tsx?$/,
-				exclude: /(node_modules|bower_components|helpers)/,
-				loader: [
-					{
-						loader: 'babel-loader',
-						options: {
-							presets: babelPresets,
-							plugins: babelPlugins,
-						},
-					}, {
-						loader: 'ts-loader',
-						options: {
-							transpileOnly: true
-						}
-					}
-				]
-			},
-			{
-				test: /\.scss$/,
-				loader: ['style-loader', 'css-loader', {
-					loader: 'sass-loader', options: {
-						implementation: require('sass')
-					}
-				}]
-			}
-		]
-
-	},
-	node: {
-		tls: 'empty',
-		fs: 'empty'
-	},
-	externals: {
-		'RestApi': 'IrisRestApiClient', //https://github.com/nRFCloud/gateway-cordova/blob/master/www/index.bundle.js
-	},
-	resolve: {
-		extensions: [".webpack.js", ".web.js", '.mjs', ".ts", ".tsx", ".js", ".json"],
-		modules: [
-			path.resolve('app'),
-			'node_modules',
-		],
-		mainFields: ['module', 'browser', 'main'],
-	},
-	plugins: [
-		new webpack.EnvironmentPlugin({...process.env}),
-		new Dotenv()
-	],
-	optimization: {
+module.exports = async env => {
+	const isProd = env.NODE_ENV === 'production';
+	const optimization = {
 		concatenateModules: true,
+	};
+	if (isProd) {
+		console.log('IS PRODUCTION!!!!');
+		optimization.minimizer = [new TerserPlugin({
+			extractComments: {
+				condition: true,
+				banner: false,
+				filename: '../../junk.js',
+			},
+			parallel: true,
+			cache: true,
+			terserOptions: {
+				safari10: true,
+				comments: false,
+			}
+		})];
 	}
+	return {
+		mode: isProd ? 'production' : 'development',
+		entry: [
+			path.resolve(__dirname, 'helpers.js'),
+			'./app/index.tsx'
+		],
+		output: {
+			filename: 'index.js',
+			path: path.resolve(__dirname, 'www/js'),
+			libraryTarget: 'this'
+		},
+		devtool: "inline-source-map",
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /(node_modules|bower_components|helpers)/,
+					loader: 'babel-loader',
+					options: {
+						presets: babelPresets,
+						plugins: babelPlugins,
+					},
+				},
+				{
+					test: /\.tsx?$/,
+					exclude: /(node_modules|bower_components|helpers)/,
+					loader: [
+						{
+							loader: 'babel-loader',
+							options: {
+								presets: babelPresets,
+								plugins: babelPlugins,
+							},
+						}, {
+							loader: 'ts-loader',
+							options: {
+								transpileOnly: true
+							}
+						}
+					]
+				},
+				{
+					test: /\.scss$/,
+					loader: ['style-loader', 'css-loader', {
+						loader: 'sass-loader', options: {
+							implementation: require('sass')
+						}
+					}]
+				}
+			]
+
+		},
+		node: {
+			tls: 'empty',
+			fs: 'empty'
+		},
+		resolve: {
+			extensions: [".webpack.js", ".web.js", '.mjs', ".ts", ".tsx", ".js", ".json"],
+			modules: [
+				path.resolve('app'),
+				'node_modules',
+			],
+			mainFields: ['module', 'browser', 'main'],
+		},
+		plugins: [
+			new webpack.LoaderOptionsPlugin({
+				debug: false
+			}),
+			new webpack.EnvironmentPlugin({ ...process.env }),
+			new Dotenv()
+		],
+		optimization: optimization
+	};
 };
